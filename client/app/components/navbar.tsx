e6f036800a18f3363c0fc9c1d920c927863d9ba5
MiniGT <bendanillo539@gmail.com>
2025-03-24 20:53:08 +0800

    style: bigger logo text and fixed padding

--- client/app/components/navbar.tsx
+++ client/app/components/navbar.tsx
@@ -22,28 +22,28 @@ export default function Navbar() {
   }
 
   return (
-    <header className="w-full px-8 text-gray-700 bg-white shadow-sm">
-      <div className="container flex flex-col md:flex-row items-center justify-between py-5 mx-auto max-w-10xl">
+    <header className="w-full text-gray-700 bg-white shadow-sm">
+      <div className="mx-15 px-4 flex flex-col justify-between md:flex-row items-center py-5">
         <div className="flex flex-col md:flex-row items-center">
           <NavLink to="/" className="flex items-center mb-5 md:mb-0">
-            <span className="text-xl font-black text-gray-900 select-none">
+            <span className="text-4xl font-black text-gray-900 select-none">
               We<span className="text-indigo-600">PhoneSpec</span>
             </span>
           </NavLink>
           <nav className="flex flex-wrap items-center ml-0 md:ml-8 md:border-l md:pl-8">
             
-          <div className="mr-5 font-medium text-gray-600 hover:text-gray-900">
-            <div className="flex items-center border border-gray-400 rounded-lg focus-within:border-gray-900">
-              <input
-                type="text"
-                className="border-none outline-none px-3 py-2 flex-1 rounded-l-lg"
-                placeholder="Search smartphone..."
-              />
-              <button type="submit" className="p-2">
-                <img src="search.svg" alt="search" className="w-5 h-5" />
-              </button>
+            <div className="mr-5 font-medium text-gray-600 hover:text-gray-900">
+              <div className="flex items-center border border-gray-400 rounded-lg focus-within:border-gray-900">
+                <input
+                  type="text"
+                  className="border-none outline-none px-3 py-2 flex-1 rounded-l-lg"
+                  placeholder="Search smartphone..."
+                />
+                <button type="submit" className="p-2">
+                  <img src="search.svg" alt="search" className="w-5 h-5" />
+                </button>
+              </div>
             </div>
-          </div>
 
             <NavLink
               to="/"
