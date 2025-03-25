b60403e08862900f48e37445a396a266cf859152
MiniGT <bendanillo539@gmail.com>
2025-03-25 21:59:47 +0800

    feat: mock data

---
+++ client/mockData.ts
@@ -0,0 +1,99 @@
+export const phones = [
+  {
+    id: 1,
+    title: "Iphone 16 Pro",
+    spotlight: "#1 Spotlight",
+    announced: "2024, September 09",
+    released: "2024, September 20",
+    description: "iPhone 16 Pro features a Grade 5 titanium design with a new, refined microblasted texture. Titanium has one of the highest strength-to-weight ratios of any metal, making these models incredibly strong and impressively light. iPhone 16 Pro comes in four stunning finishes — including new Desert Titanium.",
+    image: "imgs/phones/iphone_16_pro.png"
+  },
+  {
+    id: 2,
+    title: "Poco F3",
+    spotlight: "#2 Spotlight",
+    announced: "2021, March 22",
+    released: "2021, March 27",
+    description: "BEAST-LIKE PERFORMANCE. With flagship 7nm process technology, the performance has been massively improved. When watching videos, playing games or surfing the webs, the processor will be one step ahead, letting you enjoy the new generation of speed.",
+    image: "imgs/phones/poco_f3.png"
+  },
+  {
+    id: 3,
+    title: "Galaxy S25 | S25+",
+    spotlight: "#3 Spotlight",
+    announced: "2025, January 22",
+    released: "2025, February 03",
+    description: "Meet Galaxy S25 and S25+. A sleek, premium design wrapped in a strong aluminum frame with an immersive screen, sophisticated camera and now slimmed down for a more comfortable grip",
+    image: "imgs/phones/samsung_s25.png"
+  },{
+    id: 4,
+    title: "Iphone 16 Pro",
+    spotlight: "#1 Spotlight",
+    announced: "2024, September 09",
+    released: "2024, September 20",
+    description: "iPhone 16 Pro features a Grade 5 titanium design with a new, refined microblasted texture. Titanium has one of the highest strength-to-weight ratios of any metal, making these models incredibly strong and impressively light. iPhone 16 Pro comes in four stunning finishes — including new Desert Titanium.",
+    image: "imgs/phones/iphone_16_pro.png"
+  },
+  {
+    id: 5,
+    title: "Poco F3",
+    spotlight: "#2 Spotlight",
+    announced: "2021, March 22",
+    released: "2021, March 27",
+    description: "BEAST-LIKE PERFORMANCE. With flagship 7nm process technology, the performance has been massively improved. When watching videos, playing games or surfing the webs, the processor will be one step ahead, letting you enjoy the new generation of speed.",
+    image: "imgs/phones/poco_f3.png"
+  },
+  {
+    id: 6,
+    title: "Galaxy S25 | S25+",
+    spotlight: "#3 Spotlight",
+    announced: "2025, January 22",
+    released: "2025, February 03",
+    description: "Meet Galaxy S25 and S25+. A sleek, premium design wrapped in a strong aluminum frame with an immersive screen, sophisticated camera and now slimmed down for a more comfortable grip",
+    image: "imgs/phones/samsung_s25.png"
+  },{
+    id: 7,
+    title: "Iphone 16 Pro",
+    spotlight: "#1 Spotlight",
+    announced: "2024, September 09",
+    released: "2024, September 20",
+    description: "iPhone 16 Pro features a Grade 5 titanium design with a new, refined microblasted texture. Titanium has one of the highest strength-to-weight ratios of any metal, making these models incredibly strong and impressively light. iPhone 16 Pro comes in four stunning finishes — including new Desert Titanium.",
+    image: "imgs/phones/iphone_16_pro.png"
+  },
+  {
+    id: 8,
+    title: "Poco F3",
+    spotlight: "#2 Spotlight",
+    announced: "2021, March 22",
+    released: "2021, March 27",
+    description: "BEAST-LIKE PERFORMANCE. With flagship 7nm process technology, the performance has been massively improved. When watching videos, playing games or surfing the webs, the processor will be one step ahead, letting you enjoy the new generation of speed.",
+    image: "imgs/phones/poco_f3.png"
+  },
+  {
+    id: 9,
+    title: "Galaxy S25 | S25+",
+    spotlight: "#3 Spotlight",
+    announced: "2025, January 22",
+    released: "2025, February 03",
+    description: "Meet Galaxy S25 and S25+. A sleek, premium design wrapped in a strong aluminum frame with an immersive screen, sophisticated camera and now slimmed down for a more comfortable grip",
+    image: "imgs/phones/samsung_s25.png"
+  },
+  {
+    id: 10,
+    title: "Galaxy S25 | S25+",
+    spotlight: "#3 Spotlight",
+    announced: "2025, January 22",
+    released: "2025, February 03",
+    description: "Meet Galaxy S25 and S25+. A sleek, premium design wrapped in a strong aluminum frame with an immersive screen, sophisticated camera and now slimmed down for a more comfortable grip",
+    image: "imgs/phones/samsung_s25.png"
+  },
+  {
+    id: 11,
+    title: "Galaxy S25 | S25+",
+    spotlight: "#3 Spotlight",
+    announced: "2025, January 22",
+    released: "2025, February 03",
+    description: "Meet Galaxy S25 and S25+. A sleek, premium design wrapped in a strong aluminum frame with an immersive screen, sophisticated camera and now slimmed down for a more comfortable grip",
+    image: "imgs/phones/samsung_s25.png"
+  }
+]
