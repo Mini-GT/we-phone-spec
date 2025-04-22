4cbf359f249488f5bacbcc4878aa610e0a8c41a1
MiniGT <bendanillo539@gmail.com>
2025-04-23 00:30:00 +0800

    refactor: created a separate component for login, register and verify

--- client/app/components/loginForm.tsx
+++ client/app/components/loginForm.tsx
@@ -1,82 +1,84 @@
 import { useState } from "react";
-import { Card, CardContent } from "./ui/card";
+import { CardContent } from "./ui/card";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { X } from "lucide-react";
-import { useLogin } from "~/context/loginContext";
+import type { LoginRegisterFormProps } from "~/types/globals.type";
 
-export default function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { isLoginClicked ,setIsLoginClicked } = useLogin();
+export default function LoginForm({ handleAuthMode }: LoginRegisterFormProps) {
+  const [loginFormData, setLoginFormData] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log("Logging in with", { email, password });
+    console.log("Logging in with:", loginFormData.email, loginFormData.password );
   };
 
-  function handleLoginClick() {
-    setIsLoginClicked(prevState => ((!prevState)))
-  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginFormData({ ...loginFormData, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div className={`flex z-1 absolute items-center justify-center min-h-screen min-w-screen backdrop-blur-xs bg-black/50`}>
-      <Card className={`relative w-full max-w-sm p-6 bg-white rounded-2xl shadow-lg ${isLoginClicked && "animate-popup-enter"}`}>
+    <CardContent>
+      <h2 className="text-2xl font-bold text-center text-gray-900">Login</h2>
+      <form onSubmit={handleSubmit} className="mt-4">
+        <div className="mb-4">
+          <Label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            value={loginFormData.email}
+            onChange={handleChange}
+            required
+            className="w-full mt-1"
+            placeholder="name@email.com"
+          />
+        </div>
+        <div className="mb-4">
+          <Label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            Password
+          </Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={loginFormData.password}
+            onChange={handleChange}
+            required
+            className="w-full mt-1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="flex justify-between items-center text-sm mb-4">
+          <a href="#" className="text-blue-600 hover:underline">
+            Forgot Password?
+          </a>
+        </div>
+        <Button type="submit" className="w-full mb-4 cursor-pointer">Login</Button>
+
+        
+      </form>
+      <div className="mt-4 flex justify-center gap-2 text-sm text-nowrap">
+        <span>Don't have an account?</span>
         <button 
-          onClick={handleLoginClick} 
-          className="absolute top-2 right-2 p-1 text-gray-600 hover:text-gray-900"
+          className="text-blue-600 hover:underline cursor-pointer"
+          onClick={() => handleAuthMode("register")}
         >
-          <X size={20} />
+          Register
+        </button> 
+        <span> or </span>  
+        <button 
+          className="text-blue-600 hover:underline cursor-pointer"
+          onClick={() => handleAuthMode("verify")}
+        >
+          Verify
         </button>
-        <CardContent>
-          <h2 className="text-2xl font-bold text-center text-gray-900">Login</h2>
-          <form onSubmit={handleSubmit} className="mt-4">
-            <div className="mb-4">
-              <Label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </Label>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="w-full mt-1"
-                placeholder="name@email.com"
-              />
-            </div>
-            <div className="mb-4">
-              <Label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
-              </Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className="w-full mt-1"
-                placeholder="Password"
-              />
-            </div>
-            <div className="flex justify-between items-center text-sm mb-4">
-              <a href="#" className="text-blue-600 hover:underline">
-                Forgot Password?
-              </a>
-            </div>
-            <Button type="submit" className="w-full mb-4 cursor-pointer">Login</Button>
-
-            <div className="mb-4 flex justify-between">
-              <span>Don't have an account?</span>
-
-              <a href="" className="hover:underline">
-                <span>Register</span>
-              </a>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+      </div>
+    </CardContent>
   );
 }
