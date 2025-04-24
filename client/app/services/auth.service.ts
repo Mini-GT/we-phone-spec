fff76c9f41ba9c9fc90bb0ea73895c00e1d931e0
MiniGT <bendanillo539@gmail.com>
2025-04-24 15:57:40 +0800

    refactor: removing constructor as we are assigning the instance directly

--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -2,14 +2,10 @@ import axios, { type AxiosInstance } from "axios";
 import type { RegisterFormType } from "~/types/globals.type";
 
 class AuthService {
-  private api: AxiosInstance;
-
-  constructor() {
-    this.api = axios.create({
-      baseURL: `${import.meta.env.VITE_SMARTPHONE_API_URL}/auth`,
-      withCredentials: true, // if using cookies
-    });
-  }
+  private api: AxiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_SMARTPHONE_API_URL}/auth`,
+    withCredentials: true, // if using cookies
+  });;
 
   async login(email: string, password: string) {
     const response = await this.api.post("/login", { email, password });
