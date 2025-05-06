debda3ef329e0d5e7450178fb7399745bd28803f
MiniGT <bendanillo539@gmail.com>
2025-05-06 23:49:50 +0800

    feat: passport middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,25 +2,39 @@ import express, { type Request, type Response } from 'express'
 import cors from 'cors'
 import smartphonesRouter from "./routes/smartphones.route"
 import authRouter from "./routes/auth.route"
+import userRouter from "./routes/user.route"
 import { connectMongoDB } from './db/connect'
-import "dotenv/config"
+import cookieParser from 'cookie-parser' 
+import session from 'express-session'
+import passport from './services/passport'
 
 const app = express()
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,   
 }))
 app.use(express.json())
+app.use(cookieParser())
+app.use(session({
+  secret: process.env.SESSION_SECRET || "my-secret-key",
+  resave: false,
+  saveUninitialized: false,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use("/api/v1/smartphones", smartphonesRouter)
 
 app.use("/api/v1/auth", authRouter)
+
+app.use("/api/v1/", userRouter)
 
 app.get('/api/v1', (req: Request, res: Response): void => {
   res.json({ message: 'Hello from Express!' })
 })
 
 app.listen(process.env.PORT, () => {
-  connectMongoDB()
+  // connectMongoDB()
   console.log(`Server running on port ${process.env.PORT}`)
 })
