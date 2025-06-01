61052c4274b3dc3ca4afb982b4363b296f458f60
MiniGT <bendanillo539@gmail.com>
2025-06-02 03:48:13 +0800

    feat: protect routes base on role and permissions

---
+++ client/app/components/protectedRoute.tsx
@@ -0,0 +1,28 @@
+import { useAuth } from "~/context/authContext";
+import type { ProtectedRouteProps } from "~/types/globals.type";
+import { hasPermission } from "~/utils/permissions";
+
+export function ProtectedRoute({ 
+  children, 
+  requiredPermission, 
+  requiredRoles, 
+  fallback = null
+}: ProtectedRouteProps) {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <>{fallback}</>;
+  }
+
+  // check role-based access
+  if (requiredRoles && !requiredRoles.includes(user.role || "USER")) {
+    return <>{fallback}</>;
+  }
+
+  // check permission-based access
+  if (requiredPermission && !hasPermission(user.role || "USER", requiredPermission)) {
+    return <>{fallback}</>;
+  }
+
+  return <>{children}</>;
+}
