b42ba1c3a72e57830fb0d6a9a28b255fc2dc73a3
MiniGT <bendanillo539@gmail.com>
2025-06-03 16:29:23 +0800

    feat: token expiration validator

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -3,6 +3,7 @@ import jwt, { type JwtPayload } from "jsonwebtoken"
 import { CustomAPIError } from "@/errors/customError";
 import { verifyJwt } from "@/utils/jwt";
 import prisma from "@/prismaClient";
+import { jwtDecode } from "jwt-decode";
 
 const jwtSecretKey = process.env.JWT_SECRET
 if(!jwtSecretKey) throw new Error("JWT secret key is empty")
@@ -10,6 +11,11 @@ if(!jwtSecretKey) throw new Error("JWT secret key is empty")
 // Define a custom type for the Request with user property
 interface AuthenticatedRequest extends Request {
   userId: JwtPayload;
+}
+
+interface DecodedToken {
+  id: string;
+  exp: number;
 }
 
 export const authentication = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
@@ -34,26 +40,34 @@ export const authentication = async (req: AuthenticatedRequest, res: Response, n
   }
 }
 
-export const requireAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
   // if(req.isAuthenticated()) {
   //   return next()
   // }
 
   // res.status(401).json({ message: "Unauthorized"})
 
-  try {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "No token provided" })
+    const token = req.cookies.token
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
 
-    // Verify token
-    const decoded = verifyJwt(token) as { id: string }
+    // get token expiration time
+    const { exp } = jwtDecode<DecodedToken>(token)
+
+    // check if token is expired
+    if(exp * 1000 <= Date.now()) {
+      // if expired clear token to logout user
+      return res.status(401).clearCookie("token").json({ message: "Token expired" });
+    }
+
+    // verify token
+    const decoded = verifyJwt(token) as DecodedToken
+
     const user = await prisma.user.findUnique({ where: { id: decoded.id } })
 
     if (!user) return res.status(401).json({ message: "User not found" })
 
-     req.user = user
+    req.user = user
     next()
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
-  }
 }
