23e1b3b879f87d653164d0cbd2f63f8a5fe5f945
MiniGT <bendanillo539@gmail.com>
2025-06-03 16:16:59 +0800

    refactor: moved logout button to authContext

--- client/app/context/authContext.tsx
+++ client/app/context/authContext.tsx
@@ -1,24 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
-import { createContext, useContext, useState } from "react";
-import type { ContextProviderProps, UserMenuProps } from "~/types/globals.type";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createContext, useContext, useEffect, useState } from "react";
+import type { ContextProviderProps, loginFormType, UserMenuProps } from "~/types/globals.type";
 import AuthService from '~/services/auth.service';
 
 type AuthContextType = {
   user: Omit<UserMenuProps, "userId"> | undefined,
   isLoading: boolean,
-  error: Error | null
+  error: Error | null,
+  handleLogout: () => Promise<void>,
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({ children }: ContextProviderProps) {
+  const queryClient = useQueryClient()
+
   const { data: user, isLoading, error } = useQuery({
     queryFn: () => AuthService.getMe(),
     queryKey: ["user"],
   })
+
+  async function handleLogout() {
+    try {
+      await AuthService.logout()
+      queryClient.clear()
+      window.location.href = "/"
+    } catch (error) {
+      console.error("Logout failed:", error)
+    }
+  }
   
   return (
-    <AuthContext value={{ user, isLoading, error }}>
+    <AuthContext value={{ user, isLoading, error, handleLogout }}>
       {children}
     </AuthContext>
   );
