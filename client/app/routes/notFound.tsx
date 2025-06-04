1f138d89d6c4f63d97aa34a58e4a25fc33098f19
MiniGT <bendanillo539@gmail.com>
2025-06-04 14:39:20 +0800

    feat: not found component

---
+++ client/app/routes/notFound.tsx
@@ -0,0 +1,15 @@
+import { AlertTriangle } from "lucide-react";
+import ErrorContent from "~/components/errorContent";
+import type { NotFoundProps } from "~/types/globals.type";
+
+export default function NotFound({
+  message = "Page Not Found",
+  details = "The page you are looking for does not exist.", 
+}: NotFoundProps) {
+  return (
+    <ErrorContent 
+      message={message} 
+      details={details}
+    />
+  );
+}
