6b2aeae2653724869525e49a1e35e82aab978067
MiniGT <bendanillo539@gmail.com>
2025-06-04 14:36:33 +0800

    feat: authorized page component

---
+++ client/app/routes/_protected/devices.tsx
@@ -0,0 +1,57 @@
+import { Smartphone } from "lucide-react";
+import type { LoaderFunctionArgs } from "react-router";
+import Spinner from "~/components/spinner";
+import UserMenuNav from "~/components/userMenuNav";
+import { useAuth } from "~/context/authContext";
+import { requireAuthCookie } from "~/utils/auth";
+import Unauthorized from "../unauthorized";
+import { hasPermission, PERMISSIONS } from "~/utils/permissions";
+import { ProtectedRoute } from "~/components/protectedRoute";
+
+export async function loader({request}: LoaderFunctionArgs) {
+  const userId = await requireAuthCookie(request);
+}
+
+export default function Devices() {
+  const { user, isLoading, error } = useAuth()
+
+  
+  if (isLoading) {
+    return (
+      <Spinner />
+    )
+  }
+  
+  if (error || !user) {
+    return (
+      <div className="min-h-screen bg-gray-800 flex items-center justify-center">
+        <p className="text-red-400">Error loading profile. Please try again.</p>
+      </div>
+    )
+  }
+
+  return (
+    <ProtectedRoute requiredRoles={["ADMIN", "MODERATOR"]}>
+      <div className="min-h-screen bg-gray-800 bg-opacity-90 flex flex-col items-center py-12 px-4">
+        <UserMenuNav
+          tab={"devices"}
+          name={user?.name}
+        />
+
+        <div className="w-full max-w-3xl bg-gray-900 rounded-lg overflow-hidden">
+          <div className="p-8">
+            <div className="flex items-center mb-8">
+              <div className="text-3xl font-bold text-white flex items-center">
+                {/* <span className="mr-3">👤</span> */}
+                {/* <Smartphone className="mr-3 h-6 w-6" /> */}
+                <Smartphone fill="#d5dbdb" strokeWidth={2} className="mr-3 w-6 h-6 text-gray-400" />
+                Devices
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </ProtectedRoute>
+  )
+}
