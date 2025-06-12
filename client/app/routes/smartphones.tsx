a21b149ba4c02c35a23683f4058a2fd81798e01b
MiniGT <bendanillo539@gmail.com>
2025-06-12 23:43:21 +0800

    feat: GET data in loaders

--- client/app/routes/smartphones.tsx
+++ client/app/routes/smartphones.tsx
@@ -1,10 +1,11 @@
 import { phones } from 'mockData';
 import { useState } from 'react';
-import { Link, NavLink } from 'react-router';
+import { Link, NavLink, useLoaderData, type LoaderFunctionArgs } from 'react-router';
 import TopTenSection from '~/components/topTenSection';
 import { formatNumberToCompact } from '~/utils/formatNumber';
 import type { Route } from './+types/smartphones';
 import Pagination from '~/components/pagination';
+import smartphoneService from '~/services/smartphone.service';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +13,22 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function loader({request}: LoaderFunctionArgs) {
+  try {
+    const response = await smartphoneService.getSmartphones()
+    if (!response || !response.data) {
+      throw new Error("Failed to fetch users");
+    }
+
+    const devices = response.data.phones
+    return devices;
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export default function Smartphones() {
-  const data = phones;
+  const data = useLoaderData<typeof loader>()
   return (
     <div className='flex gap-4 mx-15 my-4'>
       {/* Smartphones List */}
