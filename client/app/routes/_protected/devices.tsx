d86c8e03d03e8ad51ecbfca442ef9468e3fc06e1
MiniGT <bendanillo539@gmail.com>
2025-06-12 23:36:03 +0800

    feat: fetch data in loader for ssr

--- client/app/routes/_protected/devices.tsx
+++ client/app/routes/_protected/devices.tsx
@@ -1,18 +1,32 @@
-import { Smartphone } from "lucide-react";
-import type { LoaderFunctionArgs } from "react-router";
+import { Smartphone, User } from "lucide-react";
+import { useLoaderData, type LoaderFunctionArgs } from "react-router";
 import Spinner from "~/components/spinner";
+import { requireAuthCookie } from "~/utils/auth";
+import { ProtectedRoute } from "~/components/protectedRoute";
+import ManagementDashoard from "~/components/managementDashboard";
+import { useSmartphone } from "~/context/smartphoneContext";
+import DevicePagination from "~/components/dashboard/deviceManagement.tsx/devicePagination";
+import smartphoneService from "~/services/smartphone.service";
 import UserMenuNav from "~/components/userMenuNav";
 import { useAuth } from "~/context/authContext";
-import { requireAuthCookie } from "~/utils/auth";
-import Unauthorized from "../unauthorized";
-import { hasPermission, PERMISSIONS } from "~/utils/permissions";
-import { ProtectedRoute } from "~/components/protectedRoute";
 
 export async function loader({request}: LoaderFunctionArgs) {
-  const userId = await requireAuthCookie(request);
+  const token = await requireAuthCookie(request);
+  try {
+    const response = await smartphoneService.getSmartphones()
+    if (!response || !response.data) {
+      throw new Error("Failed to fetch users");
+    }
+
+    const devices = response.data.phones
+    return devices;
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export default function Devices() {
+  const devices = useLoaderData<typeof loader>()
   const { user, isLoading, error } = useAuth()
 
   
@@ -32,21 +46,24 @@ export default function Devices() {
 
   return (
     <ProtectedRoute requiredRoles={["ADMIN", "MODERATOR"]}>
-      <div className="min-h-screen bg-gray-800 bg-opacity-90 flex flex-col items-center py-12 px-4">
+      <div className="min-h-screen bg-gray-800 bg-opacity-90 flex flex-col items-center py-12 px-15">
         <UserMenuNav
           tab={"devices"}
           name={user?.name}
         />
 
-        <div className="w-full max-w-3xl bg-gray-900 rounded-lg overflow-hidden">
+        <div className="w-full bg-gray-900 rounded-lg overflow-hidden">
           <div className="p-8">
-            <div className="flex items-center mb-8">
+            <div className="flex flex-col items-center mb-8">
               <div className="text-3xl font-bold text-white flex items-center">
                 {/* <span className="mr-3">👤</span> */}
                 {/* <Smartphone className="mr-3 h-6 w-6" /> */}
                 <Smartphone fill="#d5dbdb" strokeWidth={2} className="mr-3 w-6 h-6 text-gray-400" />
                 Devices
               </div>
+              <DevicePagination
+                items={devices}
+              />
             </div>
 
           </div>
