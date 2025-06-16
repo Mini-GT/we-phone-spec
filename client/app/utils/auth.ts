34b89c57e187d3dfeae87733e61bba3ca8ff8a33
MiniGT <bendanillo539@gmail.com>
2025-06-16 22:32:51 +0800

    refactor: enhance requireAuthCookie to validate userCookie before checking token expiration

--- client/app/utils/auth.ts
+++ client/app/utils/auth.ts
@@ -1,9 +1,14 @@
 import { redirect } from "react-router";
+import { isTokenValid } from "./tokenValidator";
 
 export async function requireAuthCookie(request: Request) {
   const userCookie = request.headers.get("Cookie");
-  
-  if (!userCookie) {
+
+  // check token expiration
+  const token = isTokenValid(userCookie || "");
+
+  // If the token is expired, redirect to unauthorized page
+  if (!token) {
     throw redirect("/unauthorized");
   }
 
