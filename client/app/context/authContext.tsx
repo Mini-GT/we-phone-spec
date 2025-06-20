668c382517466712005aa1e83b4484d0f7c75b10
MiniGT <bendanillo539@gmail.com>
2025-06-20 23:52:42 +0800

    refactor: update AuthContext to include setUser and remove unused variables

--- client/app/context/authContext.tsx
+++ client/app/context/authContext.tsx
@@ -1,31 +1,42 @@
 import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, type Dispatch, type SetStateAction } from "react";
 import type { ContextProviderProps, loginFormType, UserMenuProps } from "~/types/globals.type";
 import AuthService from '~/services/auth.service';
 
 type AuthContextType = {
-  user: Omit<UserMenuProps, "userId"> | undefined,
-  isLoading: boolean,
-  error: Error | null,
-  handleLogout: () => Promise<void>,
+  user:  UserMenuProps | null
+  // isLoading: boolean,
+  // error: Error | null,
+  setUser: Dispatch<SetStateAction<UserMenuProps | null>>
+  handleLogout: () => Promise<void>
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({ children }: ContextProviderProps) {
-  const queryClient = useQueryClient()
+  const [user, setUser] = useState<UserMenuProps | null>(null)
+  
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+  // const queryClient = useQueryClient()
 
-  const { data: user, isLoading, error } = useQuery({
-    queryFn: () => AuthService.getMe(),
-    queryKey: ["user"],
-    staleTime: 1000 * 60 * 1, // keeps data fresh for 1 minute
-    refetchOnWindowFocus: false, // dont refetch on tab switch
-  })
+  // const { data: user, isLoading, error } = useQuery({
+  //   queryFn: () => AuthService.getMe(),
+  //   queryKey: ["user"],
+  //   staleTime: 1000 * 60 * 1, // keeps data fresh for 1 minute
+  //   refetchOnWindowFocus: false, // dont refetch on tab switch
+  // })
 
   async function handleLogout() {
     try {
       await AuthService.logout()
-      queryClient.clear()
+      // queryClient.clear()
+      localStorage.removeItem("user")
+      setUser(null)
       window.location.href = "/"
     } catch (error) {
       console.error("Logout failed:", error)
@@ -33,7 +44,7 @@ export function AuthProvider({ children }: ContextProviderProps) {
   }
   
   return (
-    <AuthContext value={{ user, isLoading, error, handleLogout }}>
+    <AuthContext value={{ user, setUser, handleLogout }}>
       {children}
     </AuthContext>
   );
