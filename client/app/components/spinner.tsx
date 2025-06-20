1098015d1756cc6e5fc4db90499eeb245e8f5462
MiniGT <bendanillo539@gmail.com>
2025-06-20 19:52:16 +0800

    refactor: update Spinner component to accept className prop and adjust layout

--- client/app/components/spinner.tsx
+++ client/app/components/spinner.tsx
@@ -1,8 +1,22 @@
-export default function Spinner() {
+// export function Spinner() {
+//   return (
+//     <div className="h-screen flex flex-col justify-center items-center">
+//       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+//       <p className="mt-4 text-blue-500">Loading...</p>
+//     </div>
+//   );
+// }
+
+type SpinnerProps = {
+  className?: string;
+}
+
+export function Spinner({ className = '' }: SpinnerProps) {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
-      <p className="mt-4 text-blue-500">Loading...</p>
+    <div className="w-full h-full flex items-center justify-center">
+      <div
+        className={`animate-spin rounded-full border-t-4 border-blue-500 border-solid aspect-square ${className}`}
+      />
     </div>
   );
-}
+};
