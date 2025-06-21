a3e881f41472f2f9a01f46fe60f3a725300463d1
MiniGT <bendanillo539@gmail.com>
2025-06-21 23:44:15 +0800

    feat: prerender pages

--- client/react-router.config.ts
+++ client/react-router.config.ts
@@ -1,7 +1,24 @@
 import type { Config } from "@react-router/dev/config";
+import type { Smartphone } from "~/types/globals.type";
+
+const smartphoneUrl = process.env.SMARTPHONE_API_URL || "http://localhost:3000/"
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+
+  async prerender() {
+    let response = await fetch(`${smartphoneUrl}/smartphones`)
+    const data = await response.json()
+    const dynamicPaths = data.phones.map(
+    (smartphone: Smartphone) => `/smartphones/${smartphone.id}`)
+
+    return [
+      "/",
+      "/about",
+      "/smartphones",
+      ...dynamicPaths
+    ]
+  },
 } satisfies Config;
