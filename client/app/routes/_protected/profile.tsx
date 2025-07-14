2456f6fac989b568b2bac5ea246b35f7700fe9df
MiniGT <bendanillo539@gmail.com>
2025-07-14 23:09:04 +0800

    feat: enhance profile management by adding changePassword functionality and improve error handling

--- client/app/routes/_protected/profile.tsx
+++ client/app/routes/_protected/profile.tsx
@@ -14,7 +14,8 @@ import type { Route } from './+types/profile';
 import type { UserType } from '~/types/globals.type';
 import { FormField } from '~/components/form/formField';
 import { Input } from '~/components/ui/input';
-import { changeName } from '~/schema/profile.schema';
+import { changeName, changePassword } from '~/schema/profile.schema';
+import { z } from "zod";
 
 export function meta({}: MetaFunction) {
   return [
@@ -26,7 +27,7 @@ export function meta({}: MetaFunction) {
 type FieldErrorTypes = {
   nameError: string
   currentPasswordError: string
-  newPasswordError: string
+  newPasswordError: string  
   confirmPasswordError: string 
 }
 const defaultFieldErrors = {
@@ -42,32 +43,43 @@ export async function action({request}: ActionFunctionArgs) {
   const rawFormData = formData.get("profileFormData") as string
   if(rawFormData) {
     const parsedData = JSON.parse(rawFormData)
-    const { id, name, currentPassword, newPassword, confirmPassword } = parsedData
-    const changeNameResult = await changeName.safeParseAsync(name)
+    const { id, name, oldName, currentPassword, newPassword, confirmPassword } = parsedData
+    const passwordFieldValid = currentPassword || newPassword || confirmPassword
 
+    // validate name input
+    const changeNameResult = await changeName.safeParseAsync(name)
     if(!changeNameResult.success) {
       const issue = changeNameResult.error?.issues[0]
       return { nameError: issue.message }
     }
 
-    const result = await userService.changeName(token, name, id)
-    console.log(result)
+    // update name only if name is valid and all password field not valid
+    if(name && !passwordFieldValid) {
+      // dont update name if there is no change
+      if(name === oldName) {
+        return
+      }
+      const result = await userService.changeName(token, name, id)
+      console.log(result)
+    }
 
-    // const changePasswordResult = 
-    // const fieldInput = {
-    //   isEmptyName: !name,
-    //   isEmptyCurrentPassword: !currentPassword,
-    //   isEmptyNewPassword: !newPassword,
-    //   isEmptyConfirmPassword: !confirmPassword
-    // }
+    // update name and password if name is valid and at least 1 password field is valid
+    if(name && passwordFieldValid) {
+      // validate all password inputs
+      const changePasswordResult = await changePassword.safeParseAsync({ currentPassword, newPassword, confirmPassword}) 
+      if(changePasswordResult.error) {
+        const flattened = z.flattenError(changePasswordResult.error)
+        const currentPasswordError = flattened.fieldErrors["currentPassword"]?.[0] ?? ""
+        const newPasswordError = flattened.fieldErrors["newPassword"]?.[0] ?? ""
+        const confirmPasswordError = flattened.fieldErrors["confirmPassword"]?.[0] ?? ""
 
-    // if(!name) return fieldInput
-    // await userService.changeName(token, name)
+        return { currentPasswordError, newPasswordError, confirmPasswordError }
+      }
 
-    // if(!currentPassword || !newPassword || !confirmPassword) {
-    //   return fieldInput 
-    // }
-
+      const result = await userService.changePassword(token, { currentPassword, newPassword, confirmPassword }, id) 
+      console.log(result)
+      // return result
+    }
   }
 }
 
@@ -83,29 +95,28 @@ export default function Profile({
 }: Route.ComponentProps) {
   const matches = useMatches()
   const user = matches[0].data as UserType
+  const [showPasswordFields, setShowPasswordFields] = useState(true);
   const [ formData, setFormData ] = useState({
     id: user.id,
     name: user.name,
+    oldName: user.name,
     currentPassword: "",
     newPassword: "",
     confirmPassword: "",
   })
-  const [ fieldError, setFieldError ] = useState<FieldErrorTypes>(defaultFieldErrors)
-  const [showFields, setShowFields] = useState(true);
-  // const [ fieldError, setFieldError ] = useState(undefined)
+  const [ fieldError, setFieldError ] = useState<Partial <FieldErrorTypes>>(defaultFieldErrors)
   useEffect(() => {
     if (actionData) {
-      const mappedErrors = {
+      const mappedErrors: Partial <FieldErrorTypes>= {
         nameError: actionData.nameError,
-        // currentPassword: actionData.currentPasswordError,
-        // newPassword: actionData.newPasswordError,
-        // confirmPassword: actionData.confirmPasswordError,
+        currentPasswordError: actionData.currentPasswordError,
+        newPasswordError: actionData.newPasswordError,
+        confirmPasswordError: actionData.confirmPasswordError,
       }
-
       setFieldError(mappedErrors)
-
-    // Clear after 3 seconds
-    const timeout = setTimeout(() => {
+      
+      // Clear after 3 seconds
+      const timeout = setTimeout(() => {
       setFieldError(defaultFieldErrors)
     }, 3000);
 
@@ -204,7 +215,7 @@ export default function Profile({
                     inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.nameError ? "border border-2 border-red-400 shake" : null }`}
                   />
                   {fieldError.nameError ?
-                  <div className="text-red-400 text-sm">{fieldError.nameError}</div> : null
+                    <div className="text-red-400 text-sm">{fieldError.nameError}</div> : null
                   }
               </div>
 
@@ -224,13 +235,13 @@ export default function Profile({
               <div className="mb-6">
                 <button 
                   className="text-gray-400 text-lg flex items-center hover:text-gray-300 cursor-pointer mb-6"
-                  onClick={() => setShowFields(!showFields)}
+                  onClick={() => setShowPasswordFields(!showPasswordFields)}
                 >
                   <Lock className="h-4 w-4 mr-2" />
                   Change password
                 </button>
                 <AnimatePresence initial={false}>
-                  {showFields && (
+                  {showPasswordFields && (
                     <motion.div
                       initial={{ height: 0, opacity: 0 }}
                       animate={{ height: "auto", opacity: 1 }}
@@ -239,33 +250,48 @@ export default function Profile({
                       className="overflow-hidden mt-4"
                     >
                       <div className="space-y-3">
-                        <FormField 
-                          label="CURRENT PASSWORD"
-                          name="currentPassword"
-                          type="password"
-                          value={formData.currentPassword}
-                          onChangeEvent={handleChange}
-                          labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
-                          inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.currentPassword ? "border border-2 border-red-400 shake" : null }`}
-                        />
-                        <FormField 
-                          label="NEW PASSWORD"
-                          name="newPassword"
-                          type="password"
-                          value={formData.newPassword}
-                          onChangeEvent={handleChange}
-                          labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
-                          inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.newPassword ? "border border-2 border-red-400 shake" : null }`}
-                        />
-                        <FormField 
-                          label="CONFIRM PASSWORD"
-                          name="confirmPassword"
-                          type="password"
-                          value={formData.confirmPassword}
-                          onChangeEvent={handleChange}
-                          labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
-                          inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.confirmPassword? "border border-2 border-red-400 shake" : null }`}
-                        />
+                        <div className="mb-6 h-20">
+                          <FormField 
+                            label="CURRENT PASSWORD"
+                            name="currentPassword"
+                            type="password"
+                            value={formData.currentPassword}
+                            onChangeEvent={handleChange}
+                            labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
+                            inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.currentPasswordError ? "border border-2 border-red-400 shake" : null }`}
+                          />
+                          {fieldError.currentPasswordError ?
+                            <div className="text-red-400 text-sm">{fieldError.currentPasswordError}</div> : null
+                          }
+                        </div>
+                        <div className="mb-6 h-20">
+                          <FormField 
+                            label="NEW PASSWORD"
+                            name="newPassword"
+                            type="password"
+                            value={formData.newPassword}
+                            onChangeEvent={handleChange}
+                            labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
+                            inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.newPasswordError ? "border border-2 border-red-400 shake" : null }`}
+                          />
+                          {fieldError.newPasswordError ?
+                            <div className="text-red-400 text-sm">{fieldError.newPasswordError}</div> : null
+                          }
+                        </div>
+                        <div className="mb-6 h-20">
+                          <FormField 
+                            label="CONFIRM PASSWORD"
+                            name="confirmPassword"
+                            type="password"
+                            value={formData.confirmPassword}
+                            onChangeEvent={handleChange}
+                            labelStyle="block text-gray-400 text-sm mb-2 uppercase tracking-wide"
+                            inputStyle={`w-full bg-gray-800 rounded px-4 py-3 text-white ${fieldError.confirmPasswordError ? "border border-2 border-red-400 shake" : null }`}
+                          />
+                          {fieldError.confirmPasswordError ?
+                            <div className="text-red-400 text-sm">{fieldError.confirmPasswordError}</div> : null
+                          }
+                        </div>
                       </div>
                     </motion.div>
                   )}
@@ -278,6 +304,7 @@ export default function Profile({
                     className="w-full bg-pink-300 hover:bg-pink-400 text-gray-900 font-medium py-3 px-4 rounded transition-colors cursor-pointer"
                     value={JSON.stringify(formData)}
                     name="profileFormData"
+                    // onClick={handleSave}
                   >
                     Save
                 </button>
