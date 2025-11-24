5f33f7401ef6d7d409fd7263178d23a93da2ca8b
Jill Ared <bendanillo539@gmail.com>
2025-11-24 20:28:06 +0800

    feat: use resend for email verification

--- server/src/controllers/email.controller.ts
+++ server/src/controllers/email.controller.ts
@@ -1,6 +1,6 @@
 import prisma from "@/prismaClient";
-import { type Request, type Response } from "express"
-import { v4 as uuidv4 } from "uuid"
+import { type Request, type Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 import emailService from "@/services/email.service";
 import type { User } from "@prisma/client";
 import { email } from "@/schemas/email.schema";
@@ -8,25 +8,29 @@ import z from "zod";
 import { resetPasswordSchema } from "@/schemas/resetPassword.schema";
 import bcrypt from "bcryptjs";
 
-const resetPassJWTSecretKey = process.env.RESET_PASSWORD_JWT_SECRET
-if(!resetPassJWTSecretKey) throw new Error("JWT secret key is empty")
+const resetPassJWTSecretKey = process.env.RESET_PASSWORD_JWT_SECRET;
+if (!resetPassJWTSecretKey) throw new Error("JWT secret key is empty");
 
 const updateUserEmailVerification = async (req: Request, res: Response) => {
   if (!req.isAuthenticated()) {
-    return res.status(401).json({ message: 'Not authenticated' })
+    return res.status(401).json({ message: "Not authenticated" });
   }
 
-  const user = req.user as User
-  const now = new Date()
-  let verificationToken = user.emailVerifyToken
-  let verificationTokenExpiry = user.emailVerifyTokenExpiry
-  const status = user.status
+  const user = req.user as User;
+  const now = new Date();
+  let verificationToken = user.emailVerifyToken;
+  let verificationTokenExpiry = user.emailVerifyTokenExpiry;
+  const status = user.status;
 
-  if(status === "verified") {
-    res.status(400).json({ message: "user already verified" })
+  if (status === "verified") {
+    res.status(400).json({ message: "user already verified" });
   }
 
-  if (!verificationToken || !verificationTokenExpiry || verificationTokenExpiry < now) {
+  if (
+    !verificationToken ||
+    !verificationTokenExpiry ||
+    verificationTokenExpiry < now
+  ) {
     // generate new token if missing or expired
     verificationToken = uuidv4();
     verificationTokenExpiry = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24h
@@ -38,33 +42,42 @@ const updateUserEmailVerification = async (req: Request, res: Response) => {
         emailVerifyToken: verificationToken,
         emailVerifyTokenExpiry: verificationTokenExpiry,
       },
-    })
+    });
   }
 
-  const result = await emailService.sendVerificationEmail(user.email, verificationToken)
+  const result = await emailService.sendVerificationEmail(
+    user.name,
+    user.email,
+    verificationToken
+  );
 
-  if(!result) {
-    return res.status(401).json({ success: false, message: "Something went wrong"})
+  if (!result) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Couldn't send email verification" });
   }
 
-  res.status(200).json({ message: "Email Verification Sent. Please check your inbox/spambox."})
-}
+  res.status(200).json({
+    success: true,
+    message: "Please check your email inbox/spambox",
+  });
+};
 
 const verifyEmail = async (req: Request, res: Response) => {
   const { emailVerifyToken } = req.query;
-  if(!emailVerifyToken || typeof emailVerifyToken !== 'string') {
-    return res.status(400).json({ message: 'Invalid token' })
+  if (!emailVerifyToken || typeof emailVerifyToken !== "string") {
+    return res.status(400).json({ message: "Invalid token" });
   }
-  
+
   const user = await prisma.user.findFirst({
     where: {
       emailVerifyToken,
       emailVerifyTokenExpiry: { gt: new Date() },
     },
-  })
+  });
 
   if (!user) {
-    return res.status(400).json({ message: 'Invalid or expired token' })
+    return res.status(400).json({ message: "Invalid or expired token" });
   }
 
   await prisma.user.update({
@@ -74,37 +87,43 @@ const verifyEmail = async (req: Request, res: Response) => {
       emailVerifyToken: null,
       emailVerifyTokenExpiry: null,
     },
-  })
+  });
 
-  res.status(200).json({ message: 'Email verified successfully!' })
-}
+  res.status(200).json({ message: "Email verified successfully!" });
+};
 
 const forgotPassword = async (req: Request, res: Response) => {
   const { email: emailData } = req.query;
-  const result = await email.safeParseAsync(emailData)
-  if(!result.success) {
-    const flattened = z.flattenError(result.error)
-    const [defaultErr, customErr] = flattened.formErrors
-    return res.status(400).json({ error: customErr ?? defaultErr })
+  const result = await email.safeParseAsync(emailData);
+  if (!result.success) {
+    const flattened = z.flattenError(result.error);
+    const [defaultErr, customErr] = flattened.formErrors;
+    return res.status(400).json({ error: customErr ?? defaultErr });
   }
 
-  const { data } = result
+  const { data } = result;
 
   const user = await prisma.user.findUnique({
     where: {
-      email: data 
-    }
-  })
+      email: data,
+    },
+  });
 
-  if(!user) {
-    return res.status(400).json({ message: "Reset password sent unsuccessful" })
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Reset password sent unsuccessful" });
   }
 
-  const now = new Date()
-  let forgotPasswordVerifyToken = user.forgotPassVerifyToken
-  let forgotPasswordTokenExpiry = user.forgotPassTokenExpiry
+  const now = new Date();
+  let forgotPasswordVerifyToken = user.forgotPassVerifyToken;
+  let forgotPasswordTokenExpiry = user.forgotPassTokenExpiry;
 
-  if (!forgotPasswordVerifyToken || !forgotPasswordTokenExpiry || forgotPasswordTokenExpiry < now) {
+  if (
+    !forgotPasswordVerifyToken ||
+    !forgotPasswordTokenExpiry ||
+    forgotPasswordTokenExpiry < now
+  ) {
     // generate new token if missing or expired
     forgotPasswordVerifyToken = uuidv4();
     forgotPasswordTokenExpiry = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24h
@@ -115,13 +134,16 @@ const forgotPassword = async (req: Request, res: Response) => {
         forgotPassVerifyToken: forgotPasswordVerifyToken,
         forgotPassTokenExpiry: forgotPasswordTokenExpiry,
       },
-    })
+    });
   }
 
-  await emailService.sendEmailForgotPassword(user.email, forgotPasswordVerifyToken)
+  await emailService.sendEmailForgotPassword(
+    user.email,
+    forgotPasswordVerifyToken
+  );
 
-  res.status(200).json({ message: "Reset password sent successfully" })
-}
+  res.status(200).json({ message: "Reset password sent successfully" });
+};
 
 const resetPassword = async (req: Request, res: Response) => {
   const { password, confirmPassword, email, resetToken } = req.body;
@@ -130,54 +152,56 @@ const resetPassword = async (req: Request, res: Response) => {
     password,
     confirmPassword,
     email,
-    resetToken
-  })
+    resetToken,
+  });
 
-  if(!result.success) {
-    const flattened = z.flattenError(result.error)
-    return res.status(400).json({ 
-      passErr: flattened.fieldErrors.password, 
-      confirmErr: flattened.fieldErrors.confirmPassword ,
+  if (!result.success) {
+    const flattened = z.flattenError(result.error);
+    return res.status(400).json({
+      passErr: flattened.fieldErrors.password,
+      confirmErr: flattened.fieldErrors.confirmPassword,
       emailErr: flattened.fieldErrors.email,
       resetTokenErr: flattened.fieldErrors.resetToken,
-    })
+    });
   }
 
-  if(result.data.password !== result.data.confirmPassword) {
-    return res.status(400).json({ message: "Passwords don't match" })
+  if (result.data.password !== result.data.confirmPassword) {
+    return res.status(400).json({ message: "Passwords don't match" });
   }
 
   const user = await prisma.user.findUnique({
     where: {
       email: email,
       forgotPassVerifyToken: resetToken,
-      forgotPassTokenExpiry: { gt: new Date() }
-    }
-  })
+      forgotPassTokenExpiry: { gt: new Date() },
+    },
+  });
 
-  if(!user) {
-    return res.status(400).json("Couldn\'t reset password please try again later")
+  if (!user) {
+    return res
+      .status(400)
+      .json("Couldn't reset password please try again later");
   }
 
-  const hashedPassword = await bcrypt.hash(result.data.password, 8)
+  const hashedPassword = await bcrypt.hash(result.data.password, 8);
 
   await prisma.user.update({
     where: {
-      email
-    }, 
+      email,
+    },
     data: {
       password: hashedPassword,
       forgotPassVerifyToken: null,
       forgotPassTokenExpiry: null,
-    }
-  })
+    },
+  });
 
-  res.status(200).json({ message: "Reset password changed successfully" })
-}
+  res.status(200).json({ message: "Reset password changed successfully" });
+};
 
 export {
   updateUserEmailVerification,
   verifyEmail,
   forgotPassword,
-  resetPassword
-}
+  resetPassword,
+};
--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -1,38 +1,69 @@
 import { productionUrl } from "@/server";
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
+import fs from "fs";
+import path from "path";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const emailTemplatePath = path.join(
+  __dirname,
+  "../templates/verificationEmail.html"
+);
+const emailTemplate = fs.readFileSync(emailTemplatePath, "utf-8");
 
 class EmailService {
   private transporter = nodemailer.createTransport({
-  host: process.env.TRANSPORT_HOST,
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.TRANSPORT_EMAIL_USER,
-    pass: process.env.TRANSPORT_EMAIL_PASSWORD,
-  },
-});
+    host: process.env.TRANSPORT_HOST,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.TRANSPORT_EMAIL_USER,
+      pass: process.env.TRANSPORT_EMAIL_PASSWORD,
+    },
+  });
 
-  async sendVerificationEmail(email: string, token: string): Promise<boolean | undefined> {
-    const verificationUrl = `${productionUrl}/email/verify-email?verifyToken=${encodeURIComponent(token)}`;
-    try {
-      await this.transporter.sendMail({
-      from: `${process.env.TRANSPORT_FROM_EMAIL} <${process.env.TRANSPORT_FROM_NAME}>`,
+  async sendVerificationEmail(
+    username: string | null,
+    email: string,
+    token: string
+  ) {
+    const verificationUrl = `${productionUrl}/email/verify-email?verifyToken=${encodeURIComponent(
+      token
+    )}`;
+
+    const htmlContent = emailTemplate
+      .replace(/John Doe/g, username || "user")
+      .replace(
+        /https:\/\/yourapp\.com\/verify\?token=abc123xyz/g,
+        verificationUrl
+      )
+      .replace(/abc123xyz789/g, token);
+
+    // const error = false;
+    const { error } = await resend.emails.send({
+      from: "WePhoneSpec <noreply@wephonespec.site>",
       to: email,
-      subject: 'Verify Your Email',
-      html: `Click <a href="${verificationUrl}">here</a> to verify your email. *If you didn\'t request this, just ignore it.*`,
-    });     
-    } catch (error) {
-      return false
+      subject: "Email verification",
+      html: htmlContent,
+    });
+
+    if (error) {
+      return false;
     }
+
+    return true;
   }
 
   async sendEmailForgotPassword(email: string, token: string): Promise<void> {
-    const verificationUrl = `${productionUrl}/password/reset?token=${encodeURIComponent(token)}&email=${encodeURIComponent(email)}`;
+    const verificationUrl = `${productionUrl}/password/reset?token=${encodeURIComponent(
+      token
+    )}&email=${encodeURIComponent(email)}`;
 
     await this.transporter.sendMail({
       from: `${process.env.TRANSPORT_FROM_EMAIL} <${process.env.TRANSPORT_FROM_NAME}>`,
       to: email,
-      subject: 'Reset Password',
+      subject: "Reset Password",
       html: `
         <p>
           Click <a href="${verificationUrl}">here</a> to reset your password (expires in 24hrs). 
